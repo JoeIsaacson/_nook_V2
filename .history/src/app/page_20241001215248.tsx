bb6ae744b9ca626c46747e7b2242841ca8a50b6b
Your Name <joey.a.isaacson@gmail.com>
2024-10-01 21:53:53 -0500

    made a new app using onchain kit

---
+++ .history/src/app/page_20241001215248.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { useAccount, useConnect, useDisconnect, useBalance } from 'wagmi'
+import { useState } from 'react'
+
+function App() {
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+  const [showBalance, setShowBalance] = useState(false)
+
+  // Use the useBalance hook
+  const { data: balance } = useBalance({
+    address: account.address,
+    watch: true,
+  })
+
+  const handleCheckBalance = () => {
+    setShowBalance(true);
+    console.log(balance);
+  }
+
+  return (
+    <>
+      <div>
+        <h2>_NOOK</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === 'connected' && (
+          <>
+            <button type="button" onClick={() => disconnect()}>
+              Disconnect
+            </button>
+            <button type="button" onClick={handleCheckBalance}>
+              Check ETH Balance
+            </button>
+            {showBalance && balance && (
+              <div>
+                Balance: {balance.formatted} {balance.symbol}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
+    </>
+  )
+}
+
+export default App

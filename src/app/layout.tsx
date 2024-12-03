6f98927d22765037d7210dd8709942f168a60f66
Your Name <joey.a.isaacson@gmail.com>
2024-12-02 16:06:50 -0800

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ import { Inter } from 'next/font/google'
 import { headers } from 'next/headers'
 import { type ReactNode } from 'react'
 import { cookieToInitialState } from 'wagmi'
-
 import { getConfig } from '../wagmi'
+
+import { Analytics } from "@vercel/analytics/react"
+
 import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
